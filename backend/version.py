d8daee532798efbe2b5194855e7926abcdac7185
mulungi precious pabire <mulungipabire11@gmail.com>
2026-02-07 22:15:23 +0300

    Add version file for MovieFlix application

---
+++ backend/version.py
@@ -0,0 +1,17 @@
+"""
+MovieFlix Version Information
+"""
+
+__version__ = "1.0.0"
+__app_name__ = "MovieFlix"
+__author__ = "MovieFlix Team"
+__license__ = "MIT"
+
+def get_version():
+    """Get the current version string."""
+    return __version__
+
+
+def get_app_name():
+    """Get the application name."""
+    return __app_name__
